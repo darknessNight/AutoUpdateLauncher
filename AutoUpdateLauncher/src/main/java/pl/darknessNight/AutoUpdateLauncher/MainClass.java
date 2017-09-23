11c0dca2f6cb9b2e2cb09582f3da854335ad55be
Pawel Przytarski <pawelprzytarski@hotmail.com>
2017-09-23 16:14:56 +0200

    CI files added

---
+++ AutoUpdateLauncher/src/main/java/pl/darknessNight/AutoUpdateLauncher/MainClass.java
@@ -0,0 +1,12 @@
+package pl.darknessNight.AutoUpdateLauncher;
+
+/**
+ * Created by az on 2017-09-23.
+ */
+
+
+public class MainClass {
+    public static void main(String[] args){
+        System.out.print("Hello");
+    }
+}
---
+++ AutoUpdatePrepareTool/src/main/java/pl/darknessNight/AutoUpdatePrepareTool/MainClass.java
@@ -0,0 +1,11 @@
+package pl.darknessNight.AutoUpdatePrepareTool;
+
+/**
+ * Created by az on 2017-09-23.
+ */
+public class MainClass {
+    public static void main(String[] args){
+        System.out.print("Hello");
+    }
+}
+
